61cd25b5f0e9d63d5b7b948f537d760f4034d574
DESKTOP-6MRTL1B\SSAFY <rlaalswo0715@naver.com>
2026-01-19 09:01:14 +0900

    merge: Master Study 병합

--- modustudy/backend/src/main/java/com/ssafy/domain/quiz/entity/QuizContestChat.java
+++ modustudy/backend/src/main/java/com/ssafy/domain/quiz/entity/QuizContestChat.java
@@ -2,6 +2,9 @@ package com.ssafy.domain.quiz.entity;
 
 import com.ssafy.common.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -13,4 +16,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class QuizContestChat extends BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
